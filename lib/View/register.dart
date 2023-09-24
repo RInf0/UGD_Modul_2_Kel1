08ccf32d9d5382196a14129d0750b33219bb9cf3
AloysiussG <aloysiusgsetog@gmail.com>
2023-09-24 14:12:10 +0700

    menambah validasi radio button dan merapikan beberapa element

--- lib/View/register.dart
+++ lib/View/register.dart
@@ -21,9 +21,10 @@ class _RegisterViewState extends State<RegisterView> {
 
   bool passwordVisible = false;
   bool? isChecked = false;
+  bool _showErrorJenisKelamin = false;
   String selectedGender = '';
 
-  setSelectedGender(String gender){
+  setSelectedGender(String gender) {
     setState(() {
       selectedGender = gender;
     });
@@ -33,171 +34,211 @@ class _RegisterViewState extends State<RegisterView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                inputForm((p0) {
-                  if (p0 == null || p0.isEmpty) {
-                    return 'Username Tidak Boleh Kosong';
-                  }
-                  if (p0.toLowerCase() == 'anjing') {
-                    return 'Tidak boleh menggunakan kata kasar';
-                  }
-                  return null;
-                },
-                    controller: usernameController,
-                    hintTxt: "Username",
-                    helperTxt: "Ucup Surucup",
-                    iconData: Icons.person),
-                inputForm(((p0) {
-                  if (p0 == null || p0.isEmpty) {
-                    return 'Email tidak boleh kosong';
-                  }
-                  if (!p0.contains('@')) {
-                    return 'Email harus menggunakan @';
-                  }
-                  return null;
-                }),
-                    controller: emailController,
-                    hintTxt: "Email",
-                    helperTxt: "ucup@gmail.com",
-                    iconData: Icons.email),
+        child: ListView(
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  inputForm((p0) {
+                    if (p0 == null || p0.isEmpty) {
+                      return 'Username Tidak Boleh Kosong';
+                    }
+                    if (p0.toLowerCase() == 'anjing') {
+                      return 'Tidak boleh menggunakan kata kasar';
+                    }
+                    return null;
+                  },
+                      controller: usernameController,
+                      hintTxt: "Username",
+                      helperTxt: "Ucup Surucup",
+                      iconData: Icons.person),
+                  inputForm(((p0) {
+                    if (p0 == null || p0.isEmpty) {
+                      return 'Email tidak boleh kosong';
+                    }
+                    if (!p0.contains('@')) {
+                      return 'Email harus menggunakan @';
+                    }
+                    return null;
+                  }),
+                      controller: emailController,
+                      hintTxt: "Email",
+                      helperTxt: "ucup@gmail.com",
+                      iconData: Icons.email),
 
-                //* Password menggunakan visible/invisible toggle
-                Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 10, right: 20),
-                  child: SizedBox(
-                    width: 400,
-                    child: TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return "Password tidak boleh kosong";
-                        }
-                        if (value.length < 5) {
-                          return 'Password minimal 5 digit';
-                        }
-                        return null;
-                      },
-                      obscureText: passwordVisible,
-                      controller: passwordController,
-                      autofocus: true,
-                      decoration: InputDecoration(
-                        border: const OutlineInputBorder(),
-                        helperText: "xxxxxxx",
-                        hintText: "Password",
-                        prefixIcon: const Icon(Icons.lock),
-                        suffixIcon: IconButton(
-                          icon: Icon(passwordVisible
-                              ? Icons.visibility
-                              : Icons.visibility_off),
-                          onPressed: () {
-                            setState(
-                              () {
-                                passwordVisible = !passwordVisible;
-                              },
-                            );
-                          },
+                  //* Password menggunakan visible/invisible toggle
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, top: 10, right: 20),
+                    child: SizedBox(
+                      width: 400,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Password tidak boleh kosong";
+                          }
+                          if (value.length < 5) {
+                            return 'Password minimal 5 digit';
+                          }
+                          return null;
+                        },
+                        obscureText: passwordVisible,
+                        controller: passwordController,
+                        autofocus: true,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          helperText: "xxxxxxx",
+                          hintText: "Password",
+                          prefixIcon: const Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                            icon: Icon(passwordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off),
+                            onPressed: () {
+                              setState(
+                                () {
+                                  passwordVisible = !passwordVisible;
+                                },
+                              );
+                            },
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-
-                Padding(
-                padding: const EdgeInsets.only(left: 20, top: 10, right: 20),
-                child: SizedBox(
-                  width: 400,
-                  child: TextFormField(
-                    validator: (value){
-                      if (value == null || value.isEmpty) {
-                        return "Date tidak boleh kosong";
-                      }
-                      return null;
-                    },
-                    controller: dateController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.calendar_today_outlined),
-                      labelText: "Select Date"
+                  // input date picker
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, top: 10, right: 20),
+                    child: SizedBox(
+                      width: 400,
+                      child: TextFormField(
+                        // hide keyboard ketika input date ditap
+                        keyboardType: TextInputType.none,
+                        readOnly: true,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Tanggal Lahir tidak boleh kosong";
+                          }
+                          return null;
+                        },
+                        controller: dateController,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.calendar_today_outlined),
+                          helperText: "19-11-2003",
+                          hintText: "Tanggal Lahir",
+                        ),
+                        onTap: () async {
+                          DateTime? pickeddate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime.now());
+                          if (pickeddate != null) {
+                            dateController.text =
+                                DateFormat('dd-MM-yyyy').format(pickeddate);
+                          }
+                        },
+                      ),
                     ),
-                    onTap: () async{
-                      DateTime? pickeddate = await showDatePicker(
-                        context: context, 
-                        initialDate: DateTime.now(), 
-                        firstDate: DateTime(2000), 
-                        lastDate: DateTime(2101)
-                      );
-                      if(pickeddate != null){
-                        dateController.text = DateFormat('yyyy-mm-dd').format(pickeddate);
-                      }
-                    },
                   ),
-                ),
-                ),
 
-                inputForm(((p0) {
-                  //* untuk menglihat contoh penggunaan regex,uncomment baris dibawah yang dicomment
-                  //* final RegExp regex = RegExp(r'^\0?[1-9]\d{1,14}$');
-                  if (p0 == null || p0.isEmpty) {
-                    return 'Nomor Telepon tidak boleh kosong';
-                  }
-                  // if(!regex.hasMatch(p0))
-                  // {
-                  // return 'Nomor Telepon tidak valid';
-                  // }
-                  return null;
-                }),
-                    controller: notelpController,
-                    hintTxt: "No Telp",
-                    helperTxt: "082123456789",
-                    iconData: Icons.phone_android),
+                  inputForm(((p0) {
+                    //* untuk menglihat contoh penggunaan regex,uncomment baris dibawah yang dicomment
+                    //* final RegExp regex = RegExp(r'^\0?[1-9]\d{1,14}$');
+                    // regular expression
+                    final RegExp regex = RegExp(r'^\0?[1-9]\d{1,14}$');
+                    if (p0 == null || p0.isEmpty) {
+                      return "Nomor Telepon tidak boleh kosong";
+                    }
+                    if (!regex.hasMatch(p0)) {
+                      return "Nomor Telepon tidak valid";
+                    }
+                    return null;
+                  }),
+                      controller: notelpController,
+                      hintTxt: "No Telp",
+                      helperTxt: "082123456789",
+                      iconData: Icons.phone_android),
 
-                Column(
+                  Column(
                     children: <Widget>[
-                      SizedBox(height: 16.0),
-                      Text('Jenis Kelamin',
-                      style: TextStyle(fontSize: 16.0),
+                      const SizedBox(height: 16.0),
+                      const Text(
+                        'Jenis Kelamin',
+                        style: TextStyle(fontSize: 16.0),
                       ),
                       RadioListTile(
-                        title: Text('Laki-laki'),
+                        title: const Text('Laki-laki'),
                         value: 'Laki-laki',
                         groupValue: selectedGender,
-                        onChanged: (value){
+                        onChanged: (value) {
                           setSelectedGender('Laki-laki');
                         },
                       ),
                       RadioListTile(
-                        title: Text('Perempuan'),
+                        title: const Text('Perempuan'),
                         value: 'Perempuan',
                         groupValue: selectedGender,
-                        onChanged: (value){
+                        onChanged: (value) {
                           setSelectedGender('Perempuan');
                         },
                       ),
-                      
                     ],
                   ),
-                
-                Center(
-                  child: CheckboxListTile(
-                    title: const Text('Apakah Anda memiliki asuransi?'),
-                    value: isChecked,
-                    onChanged: (bool? newValue){
-                      setState(() {
-                        isChecked = newValue;
-                      });
-                    },
-                    activeColor: Colors.white,
-                    checkColor: Colors.blue,
-                    controlAffinity: ListTileControlAffinity.leading,
-                  ),
-                ),
+                  if (_showErrorJenisKelamin)
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 32),
+                          child: Text(
+                            'Jenis Kelamin harus dipilih!',
+                            style:
+                                TextStyle(color: Colors.red[700], fontSize: 12),
+                          ),
+                        )
+                      ],
+                    ),
 
-                ElevatedButton(
+                  Center(
+                    child: CheckboxListTile(
+                      title: const Text('Apakah Anda memiliki asuransi BPJS?'),
+                      value: isChecked,
+                      onChanged: (bool? newValue) {
+                        setState(() {
+                          isChecked = newValue;
+                        });
+                      },
+                      activeColor: Colors.white,
+                      checkColor: Colors.blue,
+                      controlAffinity: ListTileControlAffinity.leading,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
                     onPressed: () {
-                      if (_formKey.currentState!.validate()) {
+                      // munculkan text validasi/error handling merah ketika radio button jenis kelamin kosong
+                      setState(() {
+                        if (selectedGender.isEmpty) {
+                          _showErrorJenisKelamin = true;
+                        } else {
+                          _showErrorJenisKelamin = false;
+                          // lalu simpan jenis kelamin
+                        }
+                      });
+
+                      // validasi form
+                      if (_formKey.currentState!.validate() &&
+                          selectedGender.isNotEmpty) {
                         // ScaffoldMessenger.of(context).showSnackBar{
                         // const SnackBar(content: Text('Processing Data))};
                         Map<String, dynamic> formData = {};
@@ -237,10 +278,12 @@ class _RegisterViewState extends State<RegisterView> {
                         );
                       }
                     },
-                    child: const Text('Register'))
-              ],
+                    child: const Text('Register'),
+                  ),
+                ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
