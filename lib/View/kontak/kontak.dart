6c0588cee32fc3a6f55eff45b6c08777e28d7d6c
Gregorius02 <gregofuchen@gmail.com>
2023-12-08 21:52:13 +0700

    ui tentang kami dan kontak

--- lib/View/kontak/kontak.dart
+++ lib/View/kontak/kontak.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:ugd_modul_2_kel1/utilities/constant.dart';
 
 class KontakView extends StatefulWidget {
-  const KontakView({super.key});
+  const KontakView({Key? key}) : super(key: key);
 
   @override
   State<KontakView> createState() => KontakViewState();
 }
 
 class KontakViewState extends State<KontakView> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +20,86 @@ class KontakViewState extends State<KontakView> {
         backgroundColor: cAccentColor,
       ),
       body: SafeArea(
-        child: ListView(
-          children: [
-            Text('kontak rs, kontak emergency, igd'),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildContactItem(
+                title: 'Kontak RS',
+                subtitle: '021-423-7890',
+              ),
+              const SizedBox(height: 16),
+              _buildContactItem(
+                title: 'Kontak Emergency',
+                subtitle: '112',
+              ),
+              const SizedBox(height: 16),
+              _buildContactItem(
+                title: 'IGD',
+                subtitle: '021-762-3210',
+              ),
+              const SizedBox(height: 16),
+              _buildContactItem(
+                title: 'Poliklinik Umum',
+                subtitle: '021-555-1234',
+              ),
+              const SizedBox(height: 16),
+              _buildContactItem(
+                title: 'Laboratorium',
+                subtitle: '021-888-5678',
+              ),
+              const SizedBox(height: 16),
+              _buildContactItem(
+                title: 'Radiologi',
+                subtitle: '021-999-0000',
+              ),
+              // Tambahkan kontak lainnya sesuai kebutuhan
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContactItem({
+    required String title,
+    required String subtitle,
+  }) {
+    return Container(
+      height: 100, // Atur tinggi sesuai kebutuhan Anda
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  Icon(Icons.phone, size: 16, color: Colors.grey),
+                  SizedBox(width: 8),
+                  Text(
+                    subtitle,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/View/tentang_kami/tentang_kami.dart
+++ lib/View/tentang_kami/tentang_kami.dart
@@ -24,11 +24,80 @@ class TentangKamiViewState extends State<TentangKamiView> {
         ),
         backgroundColor: cAccentColor,
       ),
-      body: SafeArea(
-        child: ListView(
-          children: [
-            Text('blablabla, sejarah ngarang, pendiri alamat, foto gedung rs'),
-          ],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Card(
+                  elevation: 5.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Selamat datang di Aplikasi Resmi Atma Hospital!',
+                          style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 16.0),
+                        Text(
+                          'Aplikasi ini adalah solusi terkini kami untuk meningkatkan akses dan pengalaman Anda dalam merawat kesehatan. Kami berkomitmen untuk memberikan layanan yang lebih baik dan lebih mudah diakses melalui platform digital.',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Card(
+                  elevation: 5.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Komitmen Kami:',
+                          style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          'Aplikasi Atma Hospital adalah wujud komitmen kami untuk memberikan pelayanan kesehatan yang lebih modern, mudah diakses, dan disesuaikan dengan kebutuhan individu Anda. Kami berharap aplikasi ini tidak hanya mempermudah pengelolaan perawatan kesehatan Anda tetapi juga meningkatkan kualitas hidup Anda secara keseluruhan.',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        SizedBox(height: 16.0),
+                        Text(
+                          'Terima kasih telah memilih Aplikasi Resmi Atma Hospital. Kami berharap Anda merasakan manfaat yang besar dari penggunaan aplikasi ini untuk kesehatan dan kesejahteraan Anda.',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        SizedBox(height: 16.0),
+                        Text(
+                          'Salam Sehat,',
+                          style: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Tim Atma Hospital',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
